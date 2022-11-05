481f185a4bbdd57e93cd86dd0784cb40e7ea7959
Felipe <felipe_bascou@hotmail.com>
2022-11-05 16:43:43 +0100

    Update Layout

--- src/data/transformedData.jsx
+++ src/data/transformedData.jsx
@@ -1,289 +1,280 @@
 import {
-	getMonthlySum,
-	getMonthlyData,
-	setGrowthRate,
-	setCostPerMetric,
-	sumValue,
-	getTotalAvg,
-} from '../helpers/helpers';
+  getMonthlySum,
+  getMonthlyData,
+  setGrowthRate,
+  setCostPerMetric,
+  sumValue,
+  getTotalAvg,
+} from '../helpers/helpers'
 
 import {
-	AiOutlineYoutube,
-	AiOutlineLinkedin,
-	AiOutlineInstagram,
-	AiOutlineFacebook,
-	AiFillTwitterSquare,
-} from 'react-icons/ai';
+  AiOutlineYoutube,
+  AiOutlineLinkedin,
+  AiOutlineInstagram,
+  AiOutlineFacebook,
+  AiFillTwitterSquare,
+} from 'react-icons/ai'
 
-import { overviewData } from './overviewData';
-import { websiteData } from './websiteData';
-import { leadsData } from './leadsData';
-import { conversionsData } from './conversionsData';
-import { facebookData } from './facebookData';
-import { linkedinData } from './linkedinData';
-import { youtubeData } from './youtubeData';
-import { twitterData } from './twitterData';
-import { instagramData } from './instagramData';
-import { websiteTrafficData } from './websiteTrafficData';
+import { overviewData } from './overviewData'
+import { websiteData } from './websiteData'
+import { leadsData } from './leadsData'
+import { conversionsData } from './conversionsData'
+import { facebookData } from './facebookData'
+import { linkedinData } from './linkedinData'
+import { youtubeData } from './youtubeData'
+import { twitterData } from './twitterData'
+import { instagramData } from './instagramData'
+import { websiteTrafficData } from './websiteTrafficData'
 
 // All Data Array
 export const allDataArray = [
-	overviewData,
-	websiteData,
-	websiteTrafficData,
-	leadsData,
-	conversionsData,
-	linkedinData,
-	youtubeData,
-	facebookData,
-	twitterData,
-	instagramData,
-];
+  overviewData,
+  websiteData,
+  websiteTrafficData,
+  leadsData,
+  conversionsData,
+  linkedinData,
+  youtubeData,
+  facebookData,
+  twitterData,
+  instagramData,
+]
 
 // Overview
 export const overviewAllData = [
-	{
-		id: 1,
-		name: 'Total Budget',
-		totalBudget: sumValue(overviewData, 'budgetTotal'),
-	},
-	{
-		id: 2,
-		name: 'Total Expense',
-		totalExpense: sumValue(overviewData, 'expenseTotal'),
-	},
-];
+  {
+    id: 1,
+    name: 'Total Budget',
+    totalBudget: sumValue(overviewData, 'budgetTotal'),
+  },
+  {
+    id: 2,
+    name: 'Total Expense',
+    totalExpense: sumValue(overviewData, 'expenseTotal'),
+  },
+]
 
 // Website
 
 export const websiteAllData = [
-	{ id: 1, name: 'Sessions', sessions: sumValue(websiteData, 'sessions') },
-];
+  { id: 1, name: 'Sessions', sessions: sumValue(websiteData, 'sessions') },
+]
 
 // Website Traffic
-export const trafficDeffectChartData = getMonthlySum(websiteTrafficData, 2022, 'deffect');
-export const [trafficDeffectLastMonthData] = trafficDeffectChartData.slice(-1);
-export const trafficDirectChartData = getMonthlySum(websiteTrafficData, 2022, 'direct');
-export const [trafficDirectLastMonthData] = trafficDirectChartData.slice(-1);
-export const trafficDisplayChartData = getMonthlySum(websiteTrafficData, 2022, 'display');
-export const [trafficDisplayLastMonthData] = trafficDisplayChartData.slice(-1);
-export const trafficEmailChartData = getMonthlySum(websiteTrafficData, 2022, 'email');
-export const [trafficEmailLastMonthData] = trafficEmailChartData.slice(-1);
-export const trafficOrganicChartData = getMonthlySum(websiteTrafficData, 2022, 'organic');
-export const [trafficOrganicLastMonthData] = trafficOrganicChartData.slice(-1);
-export const trafficOtherChartData = getMonthlySum(websiteTrafficData, 2022, 'other');
-export const [trafficOtherLastMonthData] = trafficOtherChartData.slice(-1);
-export const trafficPaidChartData = getMonthlySum(websiteTrafficData, 2022, 'paid');
-export const [trafficPaidLastMonthData] = trafficPaidChartData.slice(-1);
-export const trafficReferralChartData = getMonthlySum(websiteTrafficData, 2022, 'referral');
-export const [trafficReferralLastMonthData] = trafficReferralChartData.slice(-1);
-export const trafficSocialChartData = getMonthlySum(websiteTrafficData, 2022, 'social');
-export const [trafficSocialLastMonthData] = trafficSocialChartData.slice(-1);
+export const trafficDeffectChartData = getMonthlySum(websiteTrafficData, 2022, 'deffect')
+export const [trafficDeffectLastMonthData] = trafficDeffectChartData.slice(-1)
+export const trafficDirectChartData = getMonthlySum(websiteTrafficData, 2022, 'direct')
+export const [trafficDirectLastMonthData] = trafficDirectChartData.slice(-1)
+export const trafficDisplayChartData = getMonthlySum(websiteTrafficData, 2022, 'display')
+export const [trafficDisplayLastMonthData] = trafficDisplayChartData.slice(-1)
+export const trafficEmailChartData = getMonthlySum(websiteTrafficData, 2022, 'email')
+export const [trafficEmailLastMonthData] = trafficEmailChartData.slice(-1)
+export const trafficOrganicChartData = getMonthlySum(websiteTrafficData, 2022, 'organic')
+export const [trafficOrganicLastMonthData] = trafficOrganicChartData.slice(-1)
+export const trafficOtherChartData = getMonthlySum(websiteTrafficData, 2022, 'other')
+export const [trafficOtherLastMonthData] = trafficOtherChartData.slice(-1)
+export const trafficPaidChartData = getMonthlySum(websiteTrafficData, 2022, 'paid')
+export const [trafficPaidLastMonthData] = trafficPaidChartData.slice(-1)
+export const trafficReferralChartData = getMonthlySum(websiteTrafficData, 2022, 'referral')
+export const [trafficReferralLastMonthData] = trafficReferralChartData.slice(-1)
+export const trafficSocialChartData = getMonthlySum(websiteTrafficData, 2022, 'social')
+export const [trafficSocialLastMonthData] = trafficSocialChartData.slice(-1)
 export const websiteTrafficAllData = [
-	{
-		id: 1,
-		name: 'Deffect',
-		lastYear: trafficDeffectChartData,
-		lastMonth: trafficDeffectLastMonthData,
-	},
-	{
-		id: 2,
-		name: 'Direct',
-		lastYear: trafficDirectChartData,
-		lastMonth: trafficDirectLastMonthData,
-	},
-	{
-		id: 3,
-		name: 'Display',
-		lastYear: trafficDisplayChartData,
-		lastMonth: trafficDisplayLastMonthData,
-	},
-	{
-		id: 4,
-		name: 'Email',
-		lastYear: trafficEmailChartData,
-		lastMonth: trafficEmailLastMonthData,
-	},
-	{
-		id: 5,
-		name: 'Organic',
-		lastYear: trafficOrganicChartData,
-		lastMonth: trafficOrganicLastMonthData,
-	},
-	{
-		id: 6,
-		name: 'Other',
-		lastYear: trafficOtherChartData,
-		lastMonth: trafficOtherLastMonthData,
-	},
-	{
-		id: 7,
-		name: 'Paid',
-		lastYear: trafficPaidChartData,
-		lastMonth: trafficPaidLastMonthData,
-	},
-	{
-		id: 8,
-		name: 'Referral',
-		lastYear: trafficReferralChartData,
-		lastMonth: trafficReferralLastMonthData,
-	},
-	{
-		id: 9,
-		name: 'Social',
-		lastYear: trafficSocialChartData,
-		lastMonth: trafficSocialLastMonthData,
-	},
-];
+  {
+    id: 1,
+    name: 'Deffect',
+    lastYear: trafficDeffectChartData,
+    lastMonth: trafficDeffectLastMonthData,
+  },
+  {
+    id: 2,
+    name: 'Direct',
+    lastYear: trafficDirectChartData,
+    lastMonth: trafficDirectLastMonthData,
+  },
+  {
+    id: 3,
+    name: 'Display',
+    lastYear: trafficDisplayChartData,
+    lastMonth: trafficDisplayLastMonthData,
+  },
+  {
+    id: 4,
+    name: 'Email',
+    lastYear: trafficEmailChartData,
+    lastMonth: trafficEmailLastMonthData,
+  },
+  {
+    id: 5,
+    name: 'Organic',
+    lastYear: trafficOrganicChartData,
+    lastMonth: trafficOrganicLastMonthData,
+  },
+  {
+    id: 6,
+    name: 'Other',
+    lastYear: trafficOtherChartData,
+    lastMonth: trafficOtherLastMonthData,
+  },
+  {
+    id: 7,
+    name: 'Paid',
+    lastYear: trafficPaidChartData,
+    lastMonth: trafficPaidLastMonthData,
+  },
+  {
+    id: 8,
+    name: 'Referral',
+    lastYear: trafficReferralChartData,
+    lastMonth: trafficReferralLastMonthData,
+  },
+  {
+    id: 9,
+    name: 'Social',
+    lastYear: trafficSocialChartData,
+    lastMonth: trafficSocialLastMonthData,
+  },
+]
 
 // Leads
-export const leadsChartData = getMonthlySum(leadsData, 2022, 'leads');
-export const leadsMonthlyData = getMonthlyData(leadsChartData);
-export const leadsGrowthRateData = setGrowthRate(leadsChartData);
-export const leadsCostChartData = getMonthlySum(leadsData, 2022, 'costPerLead');
-export const [leadsCostLastMonthData] = leadsCostChartData.slice(-1);
-export const leadsCostGrowthRateData = setGrowthRate(leadsCostChartData);
-export const costPerLeadsMetric = setCostPerMetric(
-	overviewData,
-	'expenseTotal',
-	leadsData,
-	'leads'
-);
+export const leadsChartData = getMonthlySum(leadsData, 2022, 'leads')
+export const leadsMonthlyData = getMonthlyData(leadsChartData)
+export const leadsGrowthRateData = setGrowthRate(leadsChartData)
+export const leadsCostChartData = getMonthlySum(leadsData, 2022, 'costPerLead')
+export const [leadsCostLastMonthData] = leadsCostChartData.slice(-1)
+export const leadsCostGrowthRateData = setGrowthRate(leadsCostChartData)
+export const costPerLeadsMetric = setCostPerMetric(overviewData, 'expenseTotal', leadsData, 'leads')
 export const leadsAllData = {
-	id: 1,
-	name: 'Leads',
-	monthlySumArray: leadsChartData,
-	monthlySumObject: leadsMonthlyData,
-	growthRate: leadsGrowthRateData,
-	costPer: costPerLeadsMetric,
-	costPerMonthlySumArray: leadsCostChartData,
-	costPerLastMonth: leadsCostLastMonthData,
-	costPerGrowthRate: leadsCostGrowthRateData,
-};
+  id: 1,
+  name: 'Leads',
+  monthlySumArray: leadsChartData,
+  monthlySumObject: leadsMonthlyData,
+  growthRate: leadsGrowthRateData,
+  costPer: costPerLeadsMetric,
+  costPerMonthlySumArray: leadsCostChartData,
+  costPerLastMonth: leadsCostLastMonthData,
+  costPerGrowthRate: leadsCostGrowthRateData,
+}
 
 // Conversions
-export const conversionsChartData = getMonthlySum(conversionsData, 2022, 'conversions');
-export const conversionsMonthlyData = getMonthlyData(conversionsChartData);
-export const conversionsGrowthRateData = setGrowthRate(conversionsChartData);
-export const conversionsCostChartData = getMonthlySum(conversionsData, 2022, 'costPerConversion');
-export const [conversionsCostLastMonthData] = conversionsCostChartData.slice(-1);
-export const conversionsCostGrowthRateData = setGrowthRate(conversionsCostChartData);
+export const conversionsChartData = getMonthlySum(conversionsData, 2022, 'conversions')
+export const conversionsMonthlyData = getMonthlyData(conversionsChartData)
+export const conversionsGrowthRateData = setGrowthRate(conversionsChartData)
+export const conversionsCostChartData = getMonthlySum(conversionsData, 2022, 'costPerConversion')
+export const [conversionsCostLastMonthData] = conversionsCostChartData.slice(-1)
+export const conversionsCostGrowthRateData = setGrowthRate(conversionsCostChartData)
 export const costPerConversionsMetric = setCostPerMetric(
-	overviewData,
-	'expenseTotal',
-	conversionsData,
-	'conversions'
-);
+  overviewData,
+  'expenseTotal',
+  conversionsData,
+  'conversions',
+)
 export const conversionsAllData = {
-	id: 2,
-	name: 'Conversions',
-	monthlySumArray: conversionsChartData,
-	monthlySumObject: conversionsMonthlyData,
-	growthRate: conversionsGrowthRateData,
-	costPer: costPerConversionsMetric,
-	costPerMonthlySumArray: conversionsCostChartData,
-	costPerLastMonth: conversionsCostLastMonthData,
-	costPerGrowthRate: conversionsCostGrowthRateData,
-};
+  id: 2,
+  name: 'Conversions',
+  monthlySumArray: conversionsChartData,
+  monthlySumObject: conversionsMonthlyData,
+  growthRate: conversionsGrowthRateData,
+  costPer: costPerConversionsMetric,
+  costPerMonthlySumArray: conversionsCostChartData,
+  costPerLastMonth: conversionsCostLastMonthData,
+  costPerGrowthRate: conversionsCostGrowthRateData,
+}
 
 // Facebook
-export const facebookTotalFollowersData = sumValue(facebookData, 'followers').toLocaleString('en');
-export const facebookTotalPageViewsData = sumValue(facebookData, 'pageViews').toLocaleString('en');
-export const facebookAvgCtrData = getTotalAvg(facebookData, 'ctr');
-export const facebookAvgErData = getTotalAvg(facebookData, 'er');
+export const facebookTotalFollowersData = sumValue(facebookData, 'followers').toLocaleString('en')
+export const facebookTotalPageViewsData = sumValue(facebookData, 'pageViews').toLocaleString('en')
+export const facebookAvgCtrData = getTotalAvg(facebookData, 'ctr')
+export const facebookAvgErData = getTotalAvg(facebookData, 'er')
 export const facebookAllData = {
-	id: 1,
-	name: 'Facebook',
-	icon: <AiOutlineFacebook />,
-	followers: facebookTotalFollowersData,
-	pageViews: facebookTotalPageViewsData,
-	ctr: facebookAvgCtrData,
-	er: facebookAvgErData,
-};
+  id: 1,
+  name: 'Facebook',
+  icon: <AiOutlineFacebook />,
+  followers: facebookTotalFollowersData,
+  pageViews: facebookTotalPageViewsData,
+  ctr: facebookAvgCtrData,
+  er: facebookAvgErData,
+}
 
 // Linkedin
-export const linkedinTotalFollowersData = sumValue(linkedinData, 'followers').toLocaleString('en');
-export const linkedinTotalPageViewsData = sumValue(linkedinData, 'pageViews').toLocaleString('en');
-export const linkedinAvgCtrData = getTotalAvg(linkedinData, 'ctr');
-export const linkedinAvgErData = getTotalAvg(linkedinData, 'er');
+export const linkedinTotalFollowersData = sumValue(linkedinData, 'followers').toLocaleString('en')
+export const linkedinTotalPageViewsData = sumValue(linkedinData, 'pageViews').toLocaleString('en')
+export const linkedinAvgCtrData = getTotalAvg(linkedinData, 'ctr')
+export const linkedinAvgErData = getTotalAvg(linkedinData, 'er')
 export const linkedinAllData = {
-	id: 2,
-	name: 'LinkedIn',
-	icon: <AiOutlineLinkedin />,
-	followers: linkedinTotalFollowersData,
-	pageViews: linkedinTotalPageViewsData,
-	ctr: linkedinAvgCtrData,
-	er: linkedinAvgErData,
-};
+  id: 2,
+  name: 'LinkedIn',
+  icon: <AiOutlineLinkedin />,
+  followers: linkedinTotalFollowersData,
+  pageViews: linkedinTotalPageViewsData,
+  ctr: linkedinAvgCtrData,
+  er: linkedinAvgErData,
+}
 
 // Youtube
-export const youtubeTotalFollowersData = sumValue(youtubeData, 'followers').toLocaleString('en');
-export const youtubeTotalPageViewsData = sumValue(youtubeData, 'pageViews').toLocaleString('en');
-export const youtubeAvgCtrData = getTotalAvg(youtubeData, 'ctr');
-export const youtubeAvgErData = getTotalAvg(youtubeData, 'er');
+export const youtubeTotalFollowersData = sumValue(youtubeData, 'followers').toLocaleString('en')
+export const youtubeTotalPageViewsData = sumValue(youtubeData, 'pageViews').toLocaleString('en')
+export const youtubeAvgCtrData = getTotalAvg(youtubeData, 'ctr')
+export const youtubeAvgErData = getTotalAvg(youtubeData, 'er')
 export const youtubeAllData = {
-	id: 3,
-	name: 'Youtube',
-	icon: <AiOutlineYoutube />,
-	followers: youtubeTotalFollowersData,
-	pageViews: youtubeTotalPageViewsData,
-	ctr: youtubeAvgCtrData,
-	er: youtubeAvgErData,
-};
+  id: 3,
+  name: 'Youtube',
+  icon: <AiOutlineYoutube />,
+  followers: youtubeTotalFollowersData,
+  pageViews: youtubeTotalPageViewsData,
+  ctr: youtubeAvgCtrData,
+  er: youtubeAvgErData,
+}
 
 // Twitter
-export const twitterTotalFollowersData = sumValue(twitterData, 'followers').toLocaleString('en');
-export const twitterTotalPageViewsData = sumValue(twitterData, 'pageViews').toLocaleString('en');
-export const twitterAvgCtrData = getTotalAvg(twitterData, 'ctr');
-export const twitterAvgErData = getTotalAvg(twitterData, 'er');
+export const twitterTotalFollowersData = sumValue(twitterData, 'followers').toLocaleString('en')
+export const twitterTotalPageViewsData = sumValue(twitterData, 'pageViews').toLocaleString('en')
+export const twitterAvgCtrData = getTotalAvg(twitterData, 'ctr')
+export const twitterAvgErData = getTotalAvg(twitterData, 'er')
 export const twitterAllData = {
-	id: 4,
-	name: 'Twitter',
-	icon: <AiFillTwitterSquare />,
-	followers: twitterTotalFollowersData,
-	pageViews: twitterTotalPageViewsData,
-	ctr: twitterAvgCtrData,
-	er: twitterAvgErData,
-};
+  id: 4,
+  name: 'Twitter',
+  icon: <AiFillTwitterSquare />,
+  followers: twitterTotalFollowersData,
+  pageViews: twitterTotalPageViewsData,
+  ctr: twitterAvgCtrData,
+  er: twitterAvgErData,
+}
 
 // Instagram
-export const instagramTotalFollowersData = sumValue(instagramData, 'followers').toLocaleString(
-	'en'
-);
-export const instagramTotalPageViewsData = sumValue(instagramData, 'pageViews').toLocaleString(
-	'en'
-);
-export const instagramAvgCtrData = getTotalAvg(instagramData, 'ctr');
-export const instagramAvgErData = getTotalAvg(instagramData, 'er');
+export const instagramTotalFollowersData = sumValue(instagramData, 'followers').toLocaleString('en')
+export const instagramTotalPageViewsData = sumValue(instagramData, 'pageViews').toLocaleString('en')
+export const instagramAvgCtrData = getTotalAvg(instagramData, 'ctr')
+export const instagramAvgErData = getTotalAvg(instagramData, 'er')
 export const instagramAllData = {
-	id: 5,
-	name: 'Instagram',
-	icon: <AiOutlineInstagram />,
-	followers: instagramTotalFollowersData,
-	pageViews: instagramTotalPageViewsData,
-	ctr: instagramAvgCtrData,
-	er: instagramAvgErData,
-};
+  id: 5,
+  name: 'Instagram',
+  icon: <AiOutlineInstagram />,
+  followers: instagramTotalFollowersData,
+  pageViews: instagramTotalPageViewsData,
+  ctr: instagramAvgCtrData,
+  er: instagramAvgErData,
+}
 
 // Social Media Data
 export const allSocialMediaData = [
-	facebookAllData,
-	linkedinAllData,
-	youtubeAllData,
-	twitterAllData,
-	instagramAllData,
-];
+  facebookAllData,
+  linkedinAllData,
+  youtubeAllData,
+  twitterAllData,
+  instagramAllData,
+]
 
 // All Data Object
 export const allDataObject = {
-	overview: overviewAllData,
-	website: websiteAllData,
-	traffic: websiteTrafficAllData,
-	leads: leadsAllData,
-	conversions: conversionsAllData,
-	linkedin: linkedinAllData,
-	youtube: youtubeAllData,
-	facebook: facebookAllData,
-	twitter: twitterAllData,
-	instagram: instagramAllData,
-};
+  overview: overviewAllData,
+  website: websiteAllData,
+  traffic: websiteTrafficAllData,
+  leads: leadsAllData,
+  conversions: conversionsAllData,
+  linkedin: linkedinAllData,
+  youtube: youtubeAllData,
+  facebook: facebookAllData,
+  twitter: twitterAllData,
+  instagram: instagramAllData,
+}
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,97 +1,91 @@
-import React from 'react';
-import { useState } from 'react';
-import BudgetExpense from '../components/BudgetExpense/BudgetExpense';
-import CardChart from '../components/CardChart/CardChart';
-import GoalTracker from '../components/GoalTracker/GoalTracker';
-import Header from '../components/Header/Header';
-import TrafficSource from '../components/TrafficSource/TrafficSource';
-import WebsiteOverview from '../components/WebsiteOverview/WebsiteOverview';
-import SocialMedia from '../components/SocialMedia/SocialMedia';
-import './Layout.css';
+import React from 'react'
+import { useState } from 'react'
+import BudgetExpense from '../components/BudgetExpense/BudgetExpense'
+import CardChart from '../components/CardChart/CardChart'
+import GoalTracker from '../components/GoalTracker/GoalTracker'
+import Header from '../components/Header/Header'
+import TrafficSource from '../components/TrafficSource/TrafficSource'
+import WebsiteOverview from '../components/WebsiteOverview/WebsiteOverview'
+import SocialMedia from '../components/SocialMedia/SocialMedia'
+import './Layout.css'
 import {
-	allDataArray,
-	leadsAllData,
-	conversionsAllData,
-	allSocialMediaData,
-	websiteTrafficAllData,
-} from '../data/transformedData';
+  allDataArray,
+  leadsAllData,
+  conversionsAllData,
+  allSocialMediaData,
+  websiteTrafficAllData,
+} from '../data/transformedData'
 
 const Layout = () => {
-	const [showModal, setShowModal] = useState('');
+  const [showModal, setShowModal] = useState('')
 
-	const handleModal = (e) => {
-		e.preventDefault();
-		console.log('hello');
-		setShowModal((show) => !show);
-	};
+  const handleModal = (e) => {
+    e.preventDefault()
+    console.log('hello')
+    setShowModal((show) => !show)
+  }
 
-	const handleCancel = () => {
-		setShowModal(false);
-	};
-	const handleAccept = () => {
-		setShowModal(false);
-	};
+  const handleCancel = () => {
+    setShowModal(false)
+  }
+  const handleAccept = () => {
+    setShowModal(false)
+  }
 
-	const colorPrimary = '#FF00FF';
-	const colorSecondary = '#800080';
+  const colorPrimary = '#FF00FF'
+  const colorSecondary = '#800080'
 
-	return (
-		<main className='main-grid-container'>
-			{/* <div className='header-grid-container area'>
-				<Header />
-			</div> */}
-			<div className='budget-expense-grid-container area'>
-				<BudgetExpense
-					data={allDataArray[0]}
-					colorPrimary={colorPrimary}
-					colorSecondary={colorSecondary}
-				/>
-			</div>
-			<div className='overview-grid-container area'>
-				<WebsiteOverview onClick={handleModal} showModal={showModal} data={allDataArray} />
-			</div>
-			<div className='lead-grid-container area'>
-				<CardChart
-					data={leadsAllData.monthlySumObject}
-					colorPrimary={colorPrimary}
-					colorSecondary={colorSecondary}
-					title='Leads'
-					growthRate={leadsAllData.growthRate}
-					cost={leadsAllData.costPerLastMonth}
-					costPerRate={leadsAllData.costPerGrowthRate}
-				/>
-			</div>
-			<div className='conversion-grid-container area'>
-				<CardChart
-					data={conversionsAllData.monthlySumObject}
-					colorPrimary={colorPrimary}
-					colorSecondary={colorSecondary}
-					title='Conversions'
-					growthRate={conversionsAllData.growthRate}
-					cost={conversionsAllData.costPerLastMonth}
-					costPerRate={conversionsAllData.costPerGrowthRate}
-				/>
-			</div>
-			<div className='social-media-grid-container area'>
-				<SocialMedia
-					title='Social Media'
-					data={allSocialMediaData}
-					colorPrimary={colorPrimary}
-				/>
-			</div>
-			<div className='traffic-source-grid-container area'>
-				<TrafficSource data={websiteTrafficAllData} />
-			</div>
-			<div className='goal-tracker-grid-container area'>
-				<GoalTracker
-					data={allDataArray}
-					colorPrimary={colorPrimary}
-					colorSecondary={colorSecondary}
-					title='Goal Tracker'
-				/>
-			</div>
-		</main>
-	);
-};
+  return (
+    <main className="main-grid-container">
+      <div className="logo area"></div>
+      <div className="budget-expense-grid-container area">
+        <BudgetExpense
+          data={allDataArray[0]}
+          colorPrimary={colorPrimary}
+          colorSecondary={colorSecondary}
+        />
+      </div>
+      <div className="overview-grid-container area">
+        <WebsiteOverview onClick={handleModal} showModal={showModal} data={allDataArray} />
+      </div>
+      <div className="lead-grid-container area">
+        <CardChart
+          data={leadsAllData.monthlySumObject}
+          colorPrimary={colorPrimary}
+          colorSecondary={colorSecondary}
+          title="Leads"
+          growthRate={leadsAllData.growthRate}
+          cost={leadsAllData.costPerLastMonth}
+          costPerRate={leadsAllData.costPerGrowthRate}
+        />
+      </div>
+      <div className="conversion-grid-container area">
+        <CardChart
+          data={conversionsAllData.monthlySumObject}
+          colorPrimary={colorPrimary}
+          colorSecondary={colorSecondary}
+          title="Conversions"
+          growthRate={conversionsAllData.growthRate}
+          cost={conversionsAllData.costPerLastMonth}
+          costPerRate={conversionsAllData.costPerGrowthRate}
+        />
+      </div>
+      <div className="social-media-grid-container area">
+        <SocialMedia title="Social Media" data={allSocialMediaData} colorPrimary={colorPrimary} />
+      </div>
+      <div className="traffic-source-grid-container area">
+        <TrafficSource data={websiteTrafficAllData} />
+      </div>
+      <div className="goal-tracker-grid-container area">
+        <GoalTracker
+          data={allDataArray}
+          colorPrimary={colorPrimary}
+          colorSecondary={colorSecondary}
+          title="Goal Tracker"
+        />
+      </div>
+    </main>
+  )
+}
 
-export default Layout;
+export default Layout
